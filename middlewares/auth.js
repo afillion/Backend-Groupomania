dc7d0330d21de029050ccd6309a40b2c91696eee
Alexis Fillion <fillion.alexis@gmail.com>
2021-09-15 22:29:53 +0200

    auth middleware: change next() for res.status to test frontend and replace !== to !=

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,14 +4,21 @@ const jwt = require('jsonwebtoken');
 // Authentification middleware : Use token in req.headers.authorization and decode it to get userId.
 // Compare with req.body.userId
 module.exports = (req, res, next) => {
+  console.log(req.body);
+  console.log(req.headers);
   try {
     const token = req.headers.authorization.split(' ')[1];
+    console.log(token);
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    console.log(decodedToken);
     const userId = decodedToken.userId;
-    if (req.body.userId && req.body.userId !== userId) {
+    console.log(typeof userId);
+    console.log(typeof req.body.userId)
+    if (req.body.userId && req.body.userId != userId) {
       throw 'Invalid user ID';
     } else {
-      next();
+      // next();
+      res.status(200).json({ message: "ok" });
     }
   } catch {
     res.status(401).json({
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const usersCtrl = require('../controllers/users');
+const auth = require('../middlewares/auth');
 
 router.post('/signup', usersCtrl.signup);
 router.post('/login', usersCtrl.login);
 router.get('', usersCtrl.getAll);
 router.get('/:id', usersCtrl.getOne);
+router.post('/test', auth);
 
 module.exports = router;
 // Define routes for each path. See methods in controllers/
